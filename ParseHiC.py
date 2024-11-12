f1ca0251b1369d598bda2ed08bf813aa8d7298c7
Zijie Jiang <jzjlab@163.com>
2024-11-12 19:57:10 +0800

    feat: init repos and add some new fun

---
+++ ParseHiC.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+@Author: Zijie Jiang
+@Contact: jzjlab@163.com
+@File: ParseHiC.py
+@Time: 2024/11/12 19:33
+@Function: Parse Hi-C data
+"""
+
+
+def parse_hic(hic):
+    pass
+
+
+def main():
+    print("")
+
+
+if __name__ == '__main__':
+    main()
